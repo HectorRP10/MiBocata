508f452650d4b26f08eed0297b97c6cbab5d59bb
Hector <hector.rovinyo@gmail.com>
2024-11-05 10:55:43 +0100

    Añadido variables getters y constructor(correcto)

--- models/UsuarioModel.php
+++ models/UsuarioModel.php
@@ -1,5 +1,51 @@
 <?php
-class Usuario{
-    
+class Usuario {
+    public $id;
+    public $contrasenya;
+    public $mac;
+    public $correo;
+    public $tipoUsuario;
+
+    const TIPOS_USUARIO = [
+        'ALUMNO' => 'alumno',
+        'COCINA' => 'cocina',
+        'ADMINISTRADOR' => 'admin'
+    ];
+
+    public function __construct($id,$contrasenya,$mac,$correo, $tipoUsuario) {
+        $this->id = $id;
+        $this->contrasenya = $contrasenya;
+        $this->mac = $mac;
+        $this->correo = $correo;
+        if (in_array($tipoUsuario, self::TIPOS_USUARIO)) {
+            $this->tipoUsuario = $tipoUsuario;
+        } else {
+            throw new Exception("Tipo de usuario inválido");
+        }
+    }
+
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTipoUsuario() {
+        return $this->tipoUsuario;
+    }
+
+    public function getCorreo() {
+        return $this->correo;
+    }
+    public function getMac() {
+        return $this->mac;
+    }
+    public function getContrasenya() {
+        return $this->contrasenya;
+    }
+
+
+
+
 }
+
 ?>
