cce13eea47f2e6e3d82b9bc56ba07011e0e10769
Hector <hector.rovinyo@gmail.com>
2024-11-06 12:22:12 +0100

    Creada funcion nuevo_pedido

--- models/PedidosModel.php
+++ models/PedidosModel.php
@@ -1,23 +1,23 @@
 <?php
-class Pedidos(){
+class Pedido{
 
 public $id;
 public $id_alumno;
-public $id_boacadillo;
+public $id_bocadillo;
 public $id_descuento;
 public $precio;
 public $fecha;
 public $retirado;
 
-public function __construct($id=null,$id_alumno=null,$id_boacadillo=null,$id_descuento=null,$precio=null,$fecha=null,$retirado=null){
+public function __construct($id=null,$id_alumno=null,$id_bocadillo=null,$id_descuento=null,$precio=null,$fecha=null,$retirado=null){
 
     $this->id = $id;
     $this->id_alumno = $id_alumno;
-    $this->id_bocadillo = $id_boacadillo;
+    $this->id_bocadillo = $id_bocadillo;
     $this->id_descuento = $id_descuento;
     $this->precio = $precio;
-    $this->fecha = $fecha;
-    $this->retirado = $retirado;
+    $this->fecha = new DateTime();
+    $this->retirado = null;
 }
 
 
@@ -42,6 +42,38 @@ public function getFecha() {
 public function getRetirado() {
     return $this->retirado;
 }
+
+
+public function nuevo_pedido(){
+    $conexion = Database::getInstance()->getConnection();
+
+    $sql="INSERT INTO pedidos (id_alumno, id_bocadillo, id_descuento, precio, fecha, retirado) VALUES (:id_alumno, :id_bocadillo, :id_descuento, :precio, :fecha, :retirado)";
+    $stmt = $conexion->prepare($sql);
+
+    //se convierte para poder insertarse en la BD
+    $fecha_convertida = $this->fecha->format('Y-m-d H:i:s');
+
+    $stmt->execute([
+        ':id_alumno' => $id_alumno,
+        ':id_bocadillo' => $id_bocadillo,
+        ':id_descuento' => $id_descuento,
+        ':precio' => $precio,
+        ':fecha'=> $fecha_convertida,
+        ':retirado'=> null
+    ]);
+
+    //Se asigna el ID del pedido
+    if ($stmt->rowCount() > 0) {
+        $this->id = $conexion->lastInsertId(); 
+        return $this;
+    } else {
+        return null;
+    }
+
+
+}
+
+
 
 
 }
